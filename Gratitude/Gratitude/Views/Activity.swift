a5cb865eff84c910662bedeae812d62fa2a939d0
William Wu <dev.wwu@gmail.com>
2021-04-10 11:20:12 -0700

    Add Activity.swift

---
+++ Gratitude/Gratitude/Views/Activity.swift
@@ -0,0 +1,20 @@
+//
+//  Activity.swift
+//  Gratitude
+//
+//  Created by Bill Wu on 4/10/21.
+//
+
+import SwiftUI
+
+struct Activity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Activity_Previews: PreviewProvider {
+    static var previews: some View {
+        Activity()
+    }
+}
