8971932c050ad4ccb7b0ac48dd6ccb1d4a8b9093
Advay Sharma <sharmaadvay0@gmail.com>
2021-04-09 21:40:46 -0700

    init

---
+++ Gratitude/Gratitude/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gratitude
+//
+//  Created by Advay Sharma on 4/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
