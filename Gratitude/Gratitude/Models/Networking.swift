64518d99d08a92df85b157aeee4414c604027467
Advay Sharma <sharmaadvay0@gmail.com>
2021-04-10 16:14:20 -0700

    Added networking to backend

---
+++ Gratitude/Gratitude/Models/Networking.swift
@@ -0,0 +1,80 @@
+//
+//  Networking.swift
+//  Gratitude
+//
+//  Created by Advay Sharma on 4/10/21.
+//
+
+import Foundation
+
+class Networking: ObservableObject {
+    @Published var posts = [Post]()
+    @Published var user:User = User(following: [], realName: "")
+    @Published var userPosts = [Post]()
+    
+    func fetchFeed(username:String) {
+        if let url = URL(string: "https://gratitude-310305.uc.r.appspot.com/api/feed/" + username) {
+            let urlSession = URLSession(configuration: .default)
+            let datatask = urlSession.dataTask(with: url) { (data, response, error) in
+                if (error == nil){
+                    let jsonDecoder = JSONDecoder()
+                    if let checkedData = data {
+                        do {
+                            let result = try jsonDecoder.decode(Feed.self, from: checkedData)
+                            DispatchQueue.main.async {
+                                self.posts = result.posts
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            datatask.resume()
+        }
+    }
+    
+    func fetchUser(username:String) {
+        if let url = URL(string: "https://gratitude-310305.uc.r.appspot.com/api/user/" + username) {
+            let urlSession = URLSession(configuration: .default)
+            let datatask = urlSession.dataTask(with: url) { (data, response, error) in
+                if (error == nil){
+                    let jsonDecoder = JSONDecoder()
+                    if let checkedData = data {
+                        do {
+                            let result = try jsonDecoder.decode(User.self, from: checkedData)
+                            DispatchQueue.main.async {
+                                self.user = result
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            datatask.resume()
+        }
+    }
+    
+    func fetchUserFeed(username:String) {
+        if let url = URL(string: "https://gratitude-310305.uc.r.appspot.com/api/post/" + username) {
+            let urlSession = URLSession(configuration: .default)
+            let datatask = urlSession.dataTask(with: url) { (data, response, error) in
+                if (error == nil){
+                    let jsonDecoder = JSONDecoder()
+                    if let checkedData = data {
+                        do {
+                            let result = try jsonDecoder.decode(Feed.self, from: checkedData)
+                            DispatchQueue.main.async {
+                                self.userPosts = result.posts
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            datatask.resume()
+        }
+    }
+}
