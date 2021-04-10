1c1693a3dcdb571f5bdf7f9272db8ede983d4a7b
William Wu <dev.wwu@gmail.com>
2021-04-10 09:08:16 -0700

    Text box on create post page

--- Gratitude/Gratitude/Views/CreatePost.swift
+++ Gratitude/Gratitude/Views/CreatePost.swift
@@ -8,28 +8,62 @@
 import SwiftUI
 
 struct CreatePost: View {
-    @State private var postText: String = ""
+    @Binding var isShown: Bool
+    @State private var postText: String = "I am thankful for..."
+    
+    init(isShown: Binding<Bool>) {
+        _isShown = isShown
+        UITextView.appearance().backgroundColor = .clear
+    }
     
     var body: some View {
             ZStack {
-                Color("BackgroundColor")
+                Color(red: 255 / 255, green: 246 / 255, blue: 225 / 255)
                     .ignoresSafeArea()
-                VStack(alignment: .leading) {
-                    TextEditor(text: $postText)
-                        .frame(height: 150.0)
-                        .border(Color.black, width: 1)
-                    Text("Mood")
-                    Text("Categories")
-                    Spacer()
+                VStack {
+                    HStack(alignment: .top) {
+                        Text("Make a post")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .padding(.leading)
+                            .padding(.top, 10)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            isShown = false
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 24, height: 24, alignment: .center)
+                                .foregroundColor(.black)
+                                .padding(.top, 10)
+                        })
+                    }
+                    .padding(.trailing, 25)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Mood")
+                        
+                        Text("Categories")
+                        
+                        TextEditor(text: $postText)
+                            .padding(.horizontal, 4.0)
+                            .frame(height: 150.0)
+                            .background(Color(red: 255/255, green: 253/255, blue: 247/255))
+                            .cornerRadius(4.0)
+                            .foregroundColor(Color.gray)
+                        
+                        Spacer()
+                    }
+                    .padding(.all)
                 }
-                .padding(.all)
-                .navigationBarTitle("Make a post")
             }
     }
 }
 
 struct CreatePost_Previews: PreviewProvider {
     static var previews: some View {
-        CreatePost()
+        CreatePost(isShown: .constant(true))
     }
 }
