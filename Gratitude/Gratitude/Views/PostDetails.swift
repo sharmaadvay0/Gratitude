24691bf0b8d63901c753b57bab16952cfb870072
Advay Sharma <sharmaadvay0@gmail.com>
2021-04-10 12:18:02 -0700

    added pages

---
+++ Gratitude/Gratitude/Views/PostDetails.swift
@@ -0,0 +1,22 @@
+//
+//  PostDetails.swift
+//  Gratitude
+//
+//  Created by Advay Sharma on 4/10/21.
+//
+
+import SwiftUI
+
+struct PostDetails: View {
+    var body: some View {
+        Button(action: {print("here")}, label: {
+            Text("Button")
+        })
+    }
+}
+
+struct PostDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        PostDetails()
+    }
+}
