94fd233bb78597cacc642c9f7c2819a94e9b5045
William Wu <dev.wwu@gmail.com>
2021-04-10 12:26:27 -0700

    Add Activity tab, add graph to Activity page

--- Gratitude/Gratitude/Views/Activity.swift
+++ Gratitude/Gratitude/Views/Activity.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct Activity: View {
+    let moodData = [0.0, 0.0, 0.0, 2.0, 2.2, 3.6, 4.4, 4.7, 3.1, 3.8, 4.0, 4.1, 4.8, 5.0, 4.7]
+    let chartStyle = ChartStyle(backgroundColor: Color.white, accentColor: Color(red: 255/255, green: 163/255, blue: 24/255), gradientColor: GradientColors.orange, textColor: Color(red: 130/255, green: 130/255, blue: 130/255), legendTextColor: Color.black, dropShadowColor: Color.black)
+    
     var body: some View {
         ZStack {
             Color(red: 255 / 255, green: 246 / 255, blue: 225 / 255)
@@ -19,12 +23,13 @@ struct Activity: View {
                         .fontWeight(.bold)
                         .padding(.leading)
                         .padding(.top, 10)
+                    Spacer()
                 }
                 .padding([.top, .trailing], 25)
                 
                 ScrollView {
                     VStack(alignment: .leading) {
-                        //
+                        BarChartView(data: ChartData(points: moodData), title: "Mood", legend: "Past month", style: chartStyle, form: ChartForm.extraLarge, dropShadow: false)
                     }
                 }
             }
