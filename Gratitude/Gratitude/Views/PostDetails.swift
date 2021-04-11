0a356870fa2aa2f8040a42a7807995a7b17a90c2
William Wu <dev.wwu@gmail.com>
2021-04-10 17:46:21 -0700

    Show single category in PostDetails

--- Gratitude/Gratitude/Views/PostDetails.swift
+++ Gratitude/Gratitude/Views/PostDetails.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct PostDetails: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-    private let categoryRows: [[String]] = [["Family", "Friends", "Nature"], ["Community", "Career", "Education"]]
+    private var description: String = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
+    private var category: String = Categories.categories[1]
+    private var moodRating: Double = 4.0
     var body: some View {
 
             
@@ -21,26 +23,15 @@ struct PostDetails: View {
                     Text("April 9th").fontWeight(.bold).font(.largeTitle).padding(.leading, 10)
                     Spacer()
                 }.padding(.leading, 25).padding(.top, 10)
-                Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit").padding([.leading,.trailing], 25).padding(.top, 10).font(.custom("details", size: 20))
+                Text(description).padding([.leading,.trailing], 25).padding(.top, 10).font(.custom("details", size: 20))
                 VStack(alignment: .leading) {
-                    Text("Categories").fontWeight(.bold).font(.title)
-                    ForEach(categoryRows, id: \.self) { categoryRow in
-                        HStack {
-                            ForEach(categoryRow, id: \.self) { category in
-                                ZStack {
-                                    Capsule()
-                                        .fill(Color(red: 242/255, green: 163/255, blue: 24/255))
-                                        .frame(height: 30.0)
-                                    Text(category)
-                                        .font(.footnote)
-                                        .fontWeight(.bold)
-                                        .foregroundColor(Color.white)
-                                }
-                            }
-                        }
-                        
+                    HStack {
+                        Text("Category:").fontWeight(.bold).font(.title)
+                        CategoryButton(selected: true, text: Categories.categoryTitles[category] ?? category)
+                        Spacer()
                     }
-                    Text("Mood Rating: 4.0").fontWeight(.bold).font(.title).padding(.top, 10)
+                    
+                    Text(String(format: "Mood Rating: %.1f", moodRating)).fontWeight(.bold).font(.title).padding(.top, 10)
                 }.padding()
                 Spacer()
             }
